a282beea31e4a9612ed561163391ffff949dbfa0
lukasz <lukasz.herok@gmail.com>
2019-02-27 22:51:57 +0100

    checking the user circuit

--- flappyq.py
+++ flappyq.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 import qiskit
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
 from qiskit import execute
@@ -6,29 +8,39 @@ qiskit.IBMQ.load_accounts()
 backend = qiskit.providers.ibmq.least_busy(qiskit.IBMQ.backends(simulator=True))
 shots = 100
 
+des_states = {'0', '1', '+x', '-x'}
 
-def check(desierd_state, counts, shots):
-    if desierd_state in counts:
-        return st[desierd_state] / shots
+
+def check(desierd_state, user_circut, qr, cr):
+    circuit = deepcopy(user_circut)
+    if '+x' == desierd_state:
+        circuit.h(qr)
+        desierd_state = '0'
+    if '-x' == desierd_state:
+        circuit.h(qr)
+        desierd_state = '1'
+
+    circuit.measure(qr, cr)
+    job = execute(circuit, backend=backend, shots=shots)
+    res = job.result().get_counts()
+
+    if desierd_state in res:
+        return res[desierd_state] / shots
     else:
         return 0
 
 
-q = QuantumRegister(2)  # |0>
-c = ClassicalRegister(2)
-qc = QuantumCircuit(q, c)
+q = QuantumRegister(1)  # |0>
+c = ClassicalRegister(1)
 
-# user interaction
-qc.x(q[1])
+# user interaction 1
+circuit = QuantumCircuit(q, c)
+circuit.x(q)
+print(check('1', circuit, q, c))
+print(check('0', circuit, q, c))
 
-# copy and measure
-qc.measure(q, c)
-job = execute(qc, backend=backend, shots=shots)
-result = job.result()
-st = result.get_counts()
-
-# check
-print(st)
-print(check('00', st, shots))
-print(check('10', st, shots))
-print(check('11', st, shots))
+# user interaction 2
+circuit = QuantumCircuit(q, c)
+circuit.h(q)
+print(check('+x', circuit, q, c))
+print(check('0', circuit, q, c))
